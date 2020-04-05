1ded153c906c8e969ca991a05c735ffc33db095b
dwibagus09 <58162245+dwibagus09@users.noreply.github.com>
2020-04-05 19:31:09 +0700

    Bagus
    ini login

---
+++ app/src/main/java/com/e_learning/login_activtiy/Dashboard_activity.java
@@ -0,0 +1,13 @@
+package com.e_learning.login_activtiy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
