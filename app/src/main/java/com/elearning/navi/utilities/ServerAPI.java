b3eac808dece34cd1c81ef209f38981fd75cda80
dwibagus09 <58162245+dwibagus09@users.noreply.github.com>
2020-04-22 12:00:05 +0700

    1

--- app/src/main/java/com/elearning/navi/utilities/ServerAPI.java
+++
@@ -1,8 +0,0 @@
-package com.elearning.navi.utilities;
-
-public class ServerAPI {
-    public static final String URL_DATA = "http://192.168.1.74/cilo/Mahasiswa/Api";
-    public static final String URL_INSERT = "http://192.168.1.74/cilo/Mahasiswa/ApiInsert";
-    public static final String URL_DELETE = "http://192.168.1.74/cilo/Mahasiswa/ApiDelete";
-    public static final String URL_UPDATE = "http://192.168.1.74/cilo/Mahasiswa/ApiUpdate";
-}
