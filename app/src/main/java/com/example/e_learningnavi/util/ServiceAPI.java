55b0a20df78caa755ebe07705d4e3fa3fb4f8c52
SyahdiAkbar99 <58156854+SyahdiAkbar99@users.noreply.github.com>
2020-05-05 10:33:30 -0700

    di reupload lagi

--- app/src/main/java/com/example/e_learningnavi/util/ServiceAPI.java
+++
@@ -1,8 +0,0 @@
-package com.example.e_learningnavi.util;
-
-public class ServiceAPI {
-    public static final String URL_DATA = "http://192.168.43.60/ci2/Mahasiswa/Api";
-    public static final String URL_INSERT = "http://192.168.43.60/ci2/Mahasiswa/ApiInsert";
-    public static final String URL_DELETE = "http://192.168.43.60/ci2/Mahasiswa/ApiDelete";
-    public static final String URL_UPDATE = "http://192.168.43.60/ci2/Mahasiswa/ApiUpdate";
-}
