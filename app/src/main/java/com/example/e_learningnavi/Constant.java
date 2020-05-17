27a948360466fdc0bd6aa783de49bb372f5b595a
SyahdiAkbar99 <58156854+SyahdiAkbar99@users.noreply.github.com>
2020-05-17 14:34:07 -0700

    deleted

--- app/src/main/java/com/example/e_learningnavi/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.example.e_learningnavi;
-
-public class Constant {
-    public static String BLOG = "https://omaetachiwalearning.blogspot.com/";
-    private static String DEVELOPS_BY = "Syahdi";
-    public static String SHARE_CONTENT = "Android App Quiz Using Database SQLite:\n" + BLOG + "\n- " + DEVELOPS_BY;
-}
